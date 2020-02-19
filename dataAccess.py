96014bedf5f7f17053c4fd3558942e277b27031b
Bernardo Sobral Werneck <bernardo.s.werneck@gmail.com>
2020-02-18 23:55:03 -0300

    Criação de endpoint para envio de questão

--- dataAccess.py
+++ dataAccess.py
@@ -1,12 +1,14 @@
 
 from json import dumps
-
 from flask import Flask, Response, request
-
 from neo4jrestclient.client import GraphDatabase, Node
+import logging
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_url_path='/static/')
 gdb = GraphDatabase("http://localhost:7474", username="neo4j", password="secret")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def get_index():
@@ -24,6 +26,17 @@ def get_search():
                             for [row] in results]),
                     mimetype="application/json")
 
+@app.route("/problem", methods=["POST"])
+@cross_origin()
+def persist_problem():
+    app.logger.info("problem")
+    problem = request.json
+    app.logger.info(problem)
+    print(problem, flush=True)
+    return "oioioi"
+
+
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(port=8080, debug=True)
